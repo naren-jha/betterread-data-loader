c3583c8fada1508982247f598dd5265367a824a0
Narendra Jha <njha.sde@gmail.com>
2022-10-24 01:43:27 +0530

    changes for connecting to datastax Cassandra

--- src/main/java/com/njha/betterreaddataloader/BetterreadDataLoaderApplication.java
+++ src/main/java/com/njha/betterreaddataloader/BetterreadDataLoaderApplication.java
@@ -1,13 +1,26 @@
 package com.njha.betterreaddataloader;
 
+import com.njha.betterreaddataloader.connection.DataStaxAstraProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import java.nio.file.Path;
 
 @SpringBootApplication
+@EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class BetterreadDataLoaderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BetterreadDataLoaderApplication.class, args);
+	}
+
+	@Bean
+	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties properties) {
+		Path bundle = properties.getSecureConnectBundle().toPath();
+		return builder -> builder.withCloudSecureConnectBundle(bundle);
 	}
 
 }
