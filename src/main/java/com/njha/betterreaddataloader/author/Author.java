b736ef5db889ff1bd5f56f2d8341ff7a155a6d97
Narendra Jha <njha.sde@gmail.com>
2022-10-30 23:50:51 +0530

    adding documentations

--- src/main/java/com/njha/betterreaddataloader/author/Author.java
+++ src/main/java/com/njha/betterreaddataloader/author/Author.java
@@ -10,6 +10,16 @@ import org.springframework.data.cassandra.core.mapping.Column;
 import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
 import org.springframework.data.cassandra.core.mapping.Table;
 
+/**
+ * This class represents information about book authors. Authors are inserted in our system first
+ * because the data dump that we are going to use for inserting books, has only author id and not their name.
+ * So we are first going to insert the mapping of author id to author object in our system, so that
+ * while inserting books, we can get author name from author id (because with books, we'd like to
+ * keep author name as well. so that we don't have to fetch author names separately when displaying book
+ * details. We would want to be able to query everything that is needed on book detail page in one query
+ * from Cassandra - that's the way you store data in Cassandra - you denormalize data as much as needed
+ * for faster access to information; that's the purpose of using Cassandra - provide faster access while operating on big data)
+ */
 @Table(value = "author_by_id")
 @Setter
 @Getter
--- src/main/java/com/njha/betterreaddataloader/book/Book.java
+++ src/main/java/com/njha/betterreaddataloader/book/Book.java
@@ -13,6 +13,13 @@ import org.springframework.data.cassandra.core.mapping.Table;
 import java.time.LocalDate;
 import java.util.List;
 
+/**
+ * This class represents books in our system. The partitioning is done on book_id because
+ * we want to be able to fetch book information by their id. i.e., given a book_id, we should be
+ * able to fetch the book info fast and display it on book detail page to users.
+ * So here Cassandra is used in a sort of key-value storage fashion.
+ * mapping is "book_id -> BookObject"
+ */
 @Table(value = "book_by_id")
 @Setter
 @Getter
